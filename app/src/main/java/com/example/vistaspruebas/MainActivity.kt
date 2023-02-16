a674dbc84226d2360ddd2c2a108343d581a5acc1
Rafael <f.rafael.temich24@gmail.com>
2023-02-15 19:14:48 -0600

    Laying out Activity to enter cliente information

--- app/src/main/java/com/example/vistaspruebas/MainActivity.kt
+++ app/src/main/java/com/example/vistaspruebas/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.vistaspruebas
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
 import com.example.vistaspruebas.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -11,6 +16,9 @@ import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import androidx.datastore.preferences.preferencesDataStore
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "USER_TOKEN")
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -39,18 +47,28 @@ class MainActivity : AppCompatActivity() {
 
         CoroutineScope(Dispatchers.IO).launch {
             val call = getRetrofit().create(APIService::class.java).login(user, password)
-            //val puppies = call.body();
-            runOnUiThread {
-                println(call.body())
-                //var something = call.execute()
-                //println(something)
-                println("IMPRIMIENDO SOMETHING VARIABLE")
+            if (call.isSuccessful) {
+                //var response: UsuarioResponse? = null
+                //runOnUiThread {
+                    //println(call.body())
+                    //println(call.body()?.token)
+                    //response = call.body()
+                    saveToken(call.body()?.token)
+                    println("IMPRIMIENDO EL TOKEN")
+                //}
             }
         }
         intent = Intent(this, RecargasFormulario::class.java)
         startActivity(intent)
         finish()
     }
+
+    private suspend fun saveToken(token: String?) {
+        dataStore.edit {preferences ->
+            preferences[stringPreferencesKey("token")] = token ?: ""
+        }
+    }
+
     private fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://demo.bustrack.mx/apsmg/api/")
